f4e9d9695fea16d93d4cadd0c0e19b677bae2a06
Yianni A <johnangelidis12@gmail.com>
2023-03-24 19:31:15 -0400

    Fetching recipes

---
+++ app/src/services/RecipeService.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const getRecipes = async () => {
+  const res = await axios.get('https://www.reddit.com/r/recipes/hot.json?limit=10');
+  return res.data;
+};
+
+export default getRecipes;
