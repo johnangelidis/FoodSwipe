b799c79dad1f5c3b2f86d59248bb517b90243f20
Yianni A <johnangelidis12@gmail.com>
2023-03-30 18:51:43 -0400

    test

--- app/src/services/RecipeService.ts
+++ app/src/services/RecipeService.ts
@@ -4,7 +4,7 @@ const getRecipes = async () => {
   const res = await axios.get('https://www.reddit.com/r/recipes/hot.json?limit=10');
   return res.data;
 };
-
+// test
 const filterRecipes = (recipesJSON: any) => recipesJSON.data.children.filter((recipe: any, index: number) => index >= 2 || !recipe.data.title.startsWith('['));
 
 export {
