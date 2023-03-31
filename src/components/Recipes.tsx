f1f44982c900c6166f101318d384e1dff535330e
Yianni A <johnangelidis12@gmail.com>
2023-03-30 23:07:37 -0400

    Fetching comments and extracting the ingredients

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -3,23 +3,24 @@ import React, { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Recipe from '../models/Recipe';
 import RecipeCard from './RecipeCard';
-import { getRecipes, filterRecipes } from '../services/RecipeService';
+import {
+  getRecipes, filterRecipes, processRecipes, getPostComments, extractIngredients,
+} from '../services/RecipeService';
 
 function Recipes() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   useEffect(() => {
     getRecipes().then((res) => {
       const filteredRecipes = filterRecipes(res);
-      const recipesFinal: Recipe[] = filteredRecipes?.map((recipe: any) => ({
-        id: recipe.data.id,
-        title: recipe.data.title,
-        author: recipe.data.author,
-        imageUrl: recipe.data.url,
-        imageWidth: recipe.data.preview.images[0].source.width,
-        imageHeight: recipe.data.preview.images[0].source.height,
-      }));
+      const recipesFinal: Recipe[] = processRecipes(filteredRecipes);
       setRecipes(recipesFinal);
-      console.log(recipesFinal);
+
+      recipesFinal.forEach((recipe) => {
+        getPostComments(recipe.id).then((response) => {
+          const ingredients: string = extractIngredients(response);
+          console.log(ingredients);
+        });
+      });
     });
   }, []);
   return (
--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Recipe from '../models/Recipe';
 
 const getRecipes = async () => {
   const res = await axios.get('https://www.reddit.com/r/recipes/hot.json?limit=10');
@@ -7,7 +8,26 @@ const getRecipes = async () => {
 
 const filterRecipes = (recipesJSON: any) => recipesJSON.data.children.filter((recipe: any, index: number) => index >= 2 || !recipe.data.title.startsWith('['));
 
+const processRecipes = (filteredRecipes: any) : Recipe[] => filteredRecipes?.map((recipe: any) => ({
+  id: recipe.data.id,
+  title: recipe.data.title,
+  author: recipe.data.author,
+  imageUrl: recipe.data.url,
+  imageWidth: recipe.data.preview.images[0].source.width,
+  imageHeight: recipe.data.preview.images[0].source.height,
+}));
+
+const getPostComments = async (postId: string) => {
+  const res = await axios.get(`https://www.reddit.com/r/recipes/comments/${postId}.json`);
+  return res.data;
+};
+
+const extractIngredients = (commentsJSON: any) => commentsJSON[1].data.children[0].data.body;
+
 export {
   getRecipes,
   filterRecipes,
+  processRecipes,
+  getPostComments,
+  extractIngredients,
 };
