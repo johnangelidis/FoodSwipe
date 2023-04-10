24731f8a3be6f62a328681d4d3b413c5405cf2f6
Yianni A <johnangelidis12@gmail.com>
2023-04-09 20:40:42 -0400

    Created login and register pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,62 @@
-import React from 'react';
+import {
+  Button, Card, CardContent, TextField, Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e:any) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e:any) => {
+    e.preventDefault();
+  };
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmit}>
+      <Card className="formContainer">
+        <Typography variant="h4" sx={{ textAlign: 'center' }}>Login</Typography>
+        <CardContent className="formContent">
+          <TextField
+            id="email"
+            name="email"
+            label="Email"
+            variant="standard"
+            value={email}
+            onChange={onChange}
+            sx={{ margin: '5px' }}
+          />
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            variant="standard"
+            value={password}
+            onChange={onChange}
+            sx={{ margin: '5px' }}
+            type="password"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ margin: '5px' }}
+            disabled={email === '' || password === ''}
+          >
+            Login
+          </Button>
+        </CardContent>
+      </Card>
+
+    </form>
   );
 }
 
