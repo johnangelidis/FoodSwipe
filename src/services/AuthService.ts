5f00f6878ce086ba3c59307c7a35e9a26659cf71
Yianni A <johnangelidis12@gmail.com>
2023-04-13 14:41:38 -0400

    Added recipe services

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import User from '../models/User';
-
-const REGISTER_API = `${process.env.REACT_APP_API_URL}/api/register`;
-const LOGIN_API = `${process.env.REACT_APP_API_URL}/api/login`;
+import { REACT_APP_LOGIN_API, REACT_APP_REGISTER_API } from '../../configVariables';
 
 const registerUser = async (user: User) => {
-  const response = await axios.post(REGISTER_API, user);
+  const response = await axios.post(REACT_APP_REGISTER_API, user);
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
@@ -13,7 +11,7 @@ const registerUser = async (user: User) => {
 };
 
 const loginUser = async (userData: any) => {
-  const response = await axios.post(LOGIN_API, userData);
+  const response = await axios.post(REACT_APP_LOGIN_API, userData);
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
---
+++ src/services/RecipeApiService.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { REACT_APP_GET_RECIPES_API, REACT_APP_REMOVE_RECIPE_API, REACT_APP_SAVE_RECIPE_API } from '../../configVariables';
+
+const getUserRecipes = async (data: any) => {
+  const response = await axios.get(REACT_APP_GET_RECIPES_API, data);
+  return response;
+};
+
+const saveRecipe = async (data: any) => {
+  const response = await axios.put(REACT_APP_SAVE_RECIPE_API, data);
+  return response;
+};
+
+const removeRecipe = async (data: any) => {
+  const response = await axios.delete(REACT_APP_REMOVE_RECIPE_API, data);
+  return response;
+};
+
+export {
+  getUserRecipes,
+  saveRecipe,
+  removeRecipe,
+};
