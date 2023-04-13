50baff32be7396cd26f621782a575621c68e7592
Yianni A <johnangelidis12@gmail.com>
2023-04-13 13:16:10 -0400

    updated user model

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Recipes from '../components/Recipes';
+import { RootState } from '../app/store';
 
 function Home() {
   const {
     user,
-  } = useSelector((state:any) => state.auth);
+  } = useSelector((state:RootState) => state.auth);
   const navigate = useNavigate();
   useEffect(() => {
     if (!user) {
